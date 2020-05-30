be1cddfa8b84ec96b889b78072fbbfab8bb7ba78
SvetlanaReutskaya <lana.reutska@gmail.com>
2020-05-30 15:55:45 +0300

    Initial Commit

---
+++ petagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  petagram
+//
+//  Created by Svetlana on 30.05.2020.
+//  Copyright © 2020 Svetlana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
